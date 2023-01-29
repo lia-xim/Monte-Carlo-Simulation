3adcd0fadaa6652e67285b37f1caa993a2b402c6
Liaxim <matthiasramahi@web.de>
2023-01-29 02:20:28 +0100

    EingabeView updated

--- src/main/java/com/example/application/views/eingabe/EingabeView.java
+++ src/main/java/com/example/application/views/eingabe/EingabeView.java
@@ -17,6 +17,8 @@ import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.IntegerField;
+import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
@@ -29,12 +31,16 @@ import com.vaadin.flow.router.RouteAlias;
 @Uses(Icon.class)
 public class EingabeView extends Div {
 
-    private TextField firstName = new TextField("First name");
-    private TextField lastName = new TextField("Last name");
-    private EmailField email = new EmailField("Email address");
-    private DatePicker dateOfBirth = new DatePicker("Birthday");
-    private PhoneNumberField phone = new PhoneNumberField("Phone number");
-    private TextField occupation = new TextField("Occupation");
+    private IntegerField startingYear = new IntegerField("Start Jahr");
+    private IntegerField endingYear = new IntegerField("End Jahr");
+    private IntegerField yearlyDeposit = new IntegerField("Jährliche Einzahlung");
+    private IntegerField yearlyWidthdraw = new IntegerField("Jährliche Auszahlung");
+    private IntegerField instances = new IntegerField("Instanzen");
+    private IntegerField startingCapital = new IntegerField("Start Kapital");
+    private IntegerField inflationRate = new IntegerField("Inflationsrate");
+    private IntegerField yieldRate = new IntegerField("Rendite");
+    private IntegerField standardDeviation  = new IntegerField("Standardabweichung");
+    private ComboBox strategy  = new ComboBox("Strategie");
 
     private Button cancel = new Button("Cancel");
     private Button save = new Button("Save");
@@ -48,8 +54,9 @@ public class EingabeView extends Div {
         add(createFormLayout());
         add(createButtonLayout());
 
-        binder.bindInstanceFields(this);
         clearForm();
+
+        setupInputFields();
 
         cancel.addClickListener(e -> clearForm());
         save.addClickListener(e -> {
@@ -57,6 +64,101 @@ public class EingabeView extends Div {
             Notification.show(binder.getBean().getClass().getSimpleName() + " details stored.");
             clearForm();
         });
+    }
+
+    private void setupInputFields() {
+        setupStartingYear();
+        setupEndingYear();
+        setupYearlyDeposit();
+        setupYearlyWidthdraw();
+        setupInstances();
+        setupStartingCapital();
+        setupInflationRate();
+        setupYieldRate();
+        setupStandardDeviation();
+        setupStrategy();
+    }
+
+    private void setupStartingYear() {
+        startingYear.setMin(999);
+        startingYear.setMax(9999);
+        startingYear.setValue(2023);
+        startingYear.setStepButtonsVisible(true);
+        startingYear.setErrorMessage("Bitte gib ein gültiges Jahr ein!");
+    }
+    private void setupEndingYear() {
+        endingYear.setMin(999);
+        endingYear.setMax(9999);
+        endingYear.setValue(2023);
+        endingYear.setStepButtonsVisible(true);
+        endingYear.setErrorMessage("Bitte gib ein gültiges Jahr ein!");
+    }
+    private void setupYearlyDeposit() {
+        yearlyDeposit.setHelperText("");
+        yearlyDeposit.setMin(0);
+        yearlyDeposit.setMax(10000000);
+        yearlyDeposit.setValue(300);
+        yearlyDeposit.setStepButtonsVisible(true);
+        yearlyDeposit.setErrorMessage("Bitte gib eine gültige Zahl ein!");
+    }
+    private void setupYearlyWidthdraw() {
+        yearlyWidthdraw.setHelperText("");
+        yearlyWidthdraw.setMin(0);
+        yearlyWidthdraw.setMax(10000000);
+        yearlyWidthdraw.setValue(40000);
+        yearlyWidthdraw.setStepButtonsVisible(true);
+        yearlyWidthdraw.setErrorMessage("Bitte gib eine gültige Zahl ein!");
+    }
+
+    private void setupInstances() {
+        instances.setHelperText("Maximal 50000");
+        instances.setMin(1);
+        instances.setMax(50000);
+        instances.setValue(1000);
+        instances.setStepButtonsVisible(true);
+        instances.setErrorMessage("Bitte gib eine gültige Zahl ein!");
+    }
+
+    private void setupStartingCapital() {
+        startingCapital.setHelperText("");
+        startingCapital.setMin(1);
+        startingCapital.setMax(1000000000);
+        startingCapital.setValue(650000);
+        startingCapital.setStepButtonsVisible(true);
+        startingCapital.setErrorMessage("Bitte gib eine gültige Zahl ein!");
+    }
+
+    private void setupInflationRate() {
+        inflationRate.setHelperText("");
+        inflationRate.setMin(0);
+        inflationRate.setMax(100000);
+        inflationRate.setValue(4);
+        inflationRate.setStepButtonsVisible(true);
+        inflationRate.setErrorMessage("Bitte gib eine gültige Zahl ein!");
+    }
+
+    private void setupYieldRate() {
+        yieldRate.setHelperText("");
+        yieldRate.setMin(0);
+        yieldRate.setMax(10000);
+        yieldRate.setValue(3);
+        yieldRate.setStepButtonsVisible(true);
+        yieldRate.setErrorMessage("Bitte gib eine gültige Zahl ein!");
+    }
+
+    private void setupStandardDeviation() {
+        standardDeviation.setHelperText("");
+        standardDeviation.setMin(0);
+        standardDeviation.setMax(100);
+        standardDeviation.setValue(9);
+        standardDeviation.setStepButtonsVisible(true);
+        standardDeviation.setErrorMessage("Bitte gib eine gültige Zahl ein!");
+    }
+
+    private void setupStrategy() {
+        strategy.setHelperText("");
+        strategy.setItems("Hohes Risiko", "Mittleres Risiko", "Niedriges Risiko");
+        strategy.setErrorMessage("Bitte gib eine gültige Zahl ein!");
     }
 
     private void clearForm() {
@@ -69,8 +171,7 @@ public class EingabeView extends Div {
 
     private Component createFormLayout() {
         FormLayout formLayout = new FormLayout();
-        email.setErrorMessage("Please enter a valid email address");
-        formLayout.add(firstName, lastName, dateOfBirth, phone, email, occupation);
+        formLayout.add(startingYear, endingYear, yearlyDeposit, yearlyWidthdraw, instances, startingCapital, inflationRate, yieldRate, standardDeviation, strategy);
         return formLayout;
     }
 
